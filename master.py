7c476cb57ae51b9ac5856ab3eafca4c8c017b973
Andre-Carrillo <carrillomonteroandre@gmail.com>
2025-01-06 14:48:59 -0500

    Added master.py file

---
+++ master.py
@@ -0,0 +1,11 @@
+# Haremos un análisis de las votaciones de los congresistas, el objetivo es este: 
+# tener una base de datos con los congresistas, su distrito electoral, su agrupación 
+# política y su bancada y las comisiones en las que está 
+#
+# |NOMBRE            |DISTRITO ELECTORAL     |BANCADA            |AGRUPACIÓN POLÍTICA    |COMISIONES         |
+#
+# Luego haremos una tabla con los votos de cada congresista
+# |ID CONGRESISTA    | VOTO 1       | VOTO 2        | VOTO 3        | ...
+
+
+
